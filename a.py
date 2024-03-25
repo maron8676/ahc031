48e970feba294688f5885aca4e5216752b969ae0
maron8676 <maron8676@gmail.com>
2024-03-26 02:16:09 +0900

    スコア計算と微改善

--- a.py
+++ a.py
@@ -1,12 +1,67 @@
 import getpass
-import itertools
-import math
-import random
 import sys
+from collections import defaultdict
 
 
 def li():
     return list(map(int, input().split()))
+
+
+def calc_cost(D, N, a_list, rects_list):
+    # 重なりがないことを確認
+    for d in range(D):
+        rects = rects_list[d]
+        for i in range(N - 1):
+            rect1 = rects[i]
+            for j in range(i + 1, N):
+                rect2 = rects[j]
+                if rect1[0] < rect2[0] < rect1[2] and rect1[1] < rect2[1] < rect1[3]:
+                    return -1
+                if rect1[0] < rect2[2] < rect1[2] and rect1[1] < rect2[3] < rect1[3]:
+                    return -1
+
+    cost = 0
+
+    h_set = set()
+    v_set = set()
+    for d in range(D):
+        # 広さ足りない分
+        a = a_list[d]
+        rects = rects_list[d]
+        for i in range(N):
+            rect = rects[i]
+            area = (rect[2] - rect[0]) * (rect[3] - rect[1])
+            cost += max(0, a[i] - area) * 100
+
+        # 壁作成と撤去
+        h_set_next = set()
+        v_set_next = set()
+        rects = rects_list[d]
+        for rect in rects:
+            if 0 < rect[0] < W:
+                for i in range(rect[1], rect[3]):
+                    h_set_next.add((rect[0] - 1) * W + i)
+            if 0 < rect[2] < W:
+                for i in range(rect[1], rect[3]):
+                    h_set_next.add((rect[2] - 1) * W + i)
+
+            if 0 < rect[1] < W:
+                for i in range(rect[0], rect[2]):
+                    v_set_next.add(i * W + rect[1])
+            if 0 < rect[3] < W:
+                for i in range(rect[0], rect[2]):
+                    v_set_next.add(i * W + rect[3])
+
+        if d != 0:
+            cost += len(h_set_next - h_set)
+            cost += len(h_set - h_set_next)
+            cost += len(v_set_next - v_set)
+            cost += len(v_set - v_set_next)
+
+        h_set = h_set_next
+        v_set = v_set_next
+
+    print(cost, file=sys.stderr)
 
 
 is_local = getpass.getuser() == "omotl"
@@ -20,17 +75,22 @@ for d in range(D):
 rect = [[] for _ in range(D)]
 for d in range(D):
     a = a_list[d]
-    a.sort(reverse=True)
+    a_rev = a[::-1]
     remain = W
     upper = 0
     for k in range(N):
-        h = a[k] // W
-        if a[k] % W != 0:
+        h = a_rev[k] // W
+        if a_rev[k] % W != 0:
             h += 1
         h = min(h, remain - (N - 1 - k))
-        rect[d].insert(0, (upper, 0, upper + h, W))
+        if k != N - 1:
+            rect[d].insert(0, (upper, 0, upper + h, W))
+        else:
+            rect[d].insert(0, (upper, 0, W, W))
         upper += h
         remain -= h
+
+calc_cost(D, N, a_list, rect)
 
 # output
 for d in range(D):
